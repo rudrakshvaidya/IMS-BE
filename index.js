722db204858126c711d6f0c2bb3054411a5feccb
drax <fruitbeer42069@gmail.com>
2025-05-31 13:57:59 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const libExpress = require('express');
+
+const server = libExpress();
+
+server.post('/users', (req, res) => {
+    console.log("user creation request reveieved");
+    res.send("User created successfully");
+})
+
+server.post('/teams', (req, res) => {
+    console.log("team creation request reveieved");
+    res.send("Team created successfully");
+})
+
+server.post('/players', (req, res) => {
+    console.log("player creation request reveieved");
+    res.send("player created successfully");
+})
+
+server.listen(8000,()=>{
+    console.log("Listening over 8k")
+})
